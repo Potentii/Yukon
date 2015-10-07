edcb3275b28eab7543b3032ab53445f5ba643071
Ralf Pablo Braga Soares <ralfmotard@gmail.com>
2015-10-07 17:26:02 -0300

    Finalizando Implementaçoes
    - Implementei intents para Exam e Questions.
    - Chamei com metodo onclick nos botoes das activities associadas.
    - Instanciei algumas classes que o guilherme criou para poder
    ultiliza-las na classe.

--- app/src/main/java/com/sharman/yukon/MainActivity.java
+++ app/src/main/java/com/sharman/yukon/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sharman.yukon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.sharman.yukon.model.Exam;
 
 public class MainActivity extends GoogleConnectActivity {
 
@@ -11,13 +14,13 @@ public class MainActivity extends GoogleConnectActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.addQuestionsBtn);
+    }
 
-        Button btn = (Button)findViewById(R.id.btVerProva);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public void callActivityExam(View view) {
+
+        Intent iCallActivityExam = new Intent(this, ExamCreateActivity.class);
+        startActivity(iCallActivityExam);
+        finish();
     }
 }
--- app/src/main/java/com/sharman/yukon/QuestionsCreateActivity.java
+++ app/src/main/java/com/sharman/yukon/QuestionsCreateActivity.java
@@ -1,7 +1,17 @@
 package com.sharman.yukon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.sharman.yukon.model.AnswerBox;
+import com.sharman.yukon.model.Exam;
+import com.sharman.yukon.model.Question;
+
+import java.util.Date;
 
 public class QuestionsCreateActivity extends GoogleConnectActivity {
 
@@ -9,6 +19,35 @@ public class QuestionsCreateActivity extends GoogleConnectActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions_create);
+
+        /*
+        Exam exam = new Exam();
+        */
+
+        // Criando pacote de dados da Name Exam "pegando da intent
+        Bundle nameData = getIntent().getExtras();
+        if(nameData == null) {
+            return;
+        }
+
+
     }
 
+    public void callsMyself(View view){
+
+        Intent iCallsMyself = new Intent(this, QuestionsCreateActivity.class);
+
+        final EditText edtquestionQuestionsInput = (EditText) findViewById(R.id.questionQuestionsInput);
+        String questionQuestions = edtquestionQuestionsInput.getText().toString();
+        final EditText edtweightQuestionsInput = (EditText) findViewById(R.id.weightQuestionsInput);
+        //Lembrando o Peso é Bundle ou float.
+        String weightQuestionsInput = edtweightQuestionsInput.getText().toString();
+        //instanciei a classe AnswerBox para poder usa-la.
+        AnswerBox answerBox = new AnswerBox();
+        Question question = new Question(questionQuestions, Double.parseDouble(weightQuestionsInput), answerBox);
+
+        iCallsMyself.putExtra("question", question.toString());
+        startActivity(iCallsMyself);
+        finish();
+    }
 }
